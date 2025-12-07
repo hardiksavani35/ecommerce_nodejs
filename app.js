95dd204ac7c2c0a2f0e3675d4c320eb27e80a3df
hardiksavani35 <hardiksavaniaus@gmail.com>
2025-12-08 10:04:28 +1030

    - Installed ejs template and configure it - Create directory for assets and pages - Configure route path

--- app.js
+++ app.js
@@ -1,8 +1,30 @@
 const express = require('express');
+const path = require('path');
 const app = express()
 
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'))
+
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.render('home')
 })
+
+app.get('/products', (req, res) => {
+    res.render('products')
+})
+
+app.get('/cart', (req, res) => {
+    res.render('cart');
+});
+
+app.get('/product/:id', (req, res) => {
+    res.render('product-detail')
+})
+
+app.use((req, res) => {
+    res.status(404).render('404');
+});
 
 app.listen(3000)
