a2b1e65b14c6436fbdc30a76630598a616eff345
hardiksavani35 <hardiksavaniaus@gmail.com>
2025-12-25 07:53:09 +1030

    - Previous commit

--- routes/index.js
+++ routes/index.js
@@ -3,28 +3,4 @@ const route   = express.Router();
 const categoryService = require('../services/categoryService');
 const productService = require('../services/productService');
 
-route.get('/', (req, res) => {
-    res.render('home', { 
-        title: 'Home',
-        categories: categoryService.getAllCategories(),
-        featuredProducts: productService.getFeaturedProducts(),
-    });
-});
-
-route.get('/products', (req, res) => {
-    res.render('products')
-})
-
-route.get('/cart', (req, res) => {
-    res.render('cart');
-});
-
-route.get('/product/:id', (req, res) => {
-    res.render('product-detail')
-})
-
-route.use((req, res) => {
-    res.status(404).render('404');
-});
-
 module.exports = route;
