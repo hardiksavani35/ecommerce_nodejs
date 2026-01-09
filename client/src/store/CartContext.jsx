b2458efe697e748e75c38bddf0e4445a497864e4
hardiksavani35 <hardiksavaniaus@gmail.com>
2026-01-09 22:50:26 +1030

    Update product quantity if the same product is added to the cart.

--- client/src/store/CartContext.jsx
+++ client/src/store/CartContext.jsx
@@ -6,8 +6,16 @@ export function CartProvider({children}) {
     const [cart, setCart] = useState([]);
 
     const addToCart = (product) => {
-        setCart((prevCart) => [...prevCart, product]);
-    }
+        setCart((prevCart) => { 
+            const existingProduct = prevCart.find((item) => item.id === product.id);
+
+            if (existingProduct) { 
+                return prevCart.map((item) => item.id === product.id ? { ...item, quantity: item.quantity + 1 } : item);
+            } else { 
+                return [...prevCart, { ...product, quantity: 1 }];
+            }
+        });
+    };
 
     return (
         <CartContext.Provider value={{ cart, addToCart }}>
